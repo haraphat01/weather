13744eaf6cf9786c60a8e6910c642e161c5dd0d9
haraphat01 <haraphat@gmail.com>
2020-10-28 21:23:07 +0100

    Worked on the TSE recommendation

--- src/app.js
+++ src/app.js
@@ -1,33 +1,3 @@
-// const app = () => {
-//   const apiRequest = new XMLHttpRequest();
-//   const parent = document.getElementById('container');
-//   parent.addEventListener('click', (e) => {
-//     if (e.target.classList.contains('submit_button')) {
-//       e.preventDefault();
-//       const cityInput = document.getElementById('city');
-//       const chosenCity = cityInput.value;
-//       apiRequest.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=11d091024281fd0f3a53ff3d3b78c460`);
-//       apiRequest.send();
-//     }
-//   });
-
-//   apiRequest.onreadystatechange = () => {
-//     const report = document.getElementById('weather_result');
-//     if (apiRequest.readyState === 4) {
-//       if (apiRequest.readyState === 404) {
-//         report.textContent = 'City not found';
-//         return report.textContent;
-//       }
-
-//       const response = JSON.parse(apiRequest.response);
-//       report.textContent = `The weather in${response.name}is${response.weather[0].main}`;
-//     }
-//     return report;
-//   };
-
-//   return apiRequest;
-// };
-
 const app = () => {
   const parent = document.getElementById('container');
   parent.addEventListener('click', (e) => {
@@ -41,11 +11,17 @@ const app = () => {
         .then((response) => response.json())
         .then((response) => {
           const report = document.getElementById('weather_result');
+          const colors = ['blue', 'red', 'yellow', 'pink'];
+          for (let i = 0; i <= colors.length; i = +1) {
+            report.style.backgroundColor = colors[i];
+          }
           report.textContent = `The weather in ${response.name}  is ${response.weather[0].main} with ${response.main.temp} °`;
         })
         .catch((error) => ((`${chosenCity} not found`, error)));
     }
   });
+
+  //  const report = document.getElementById('weather_result');
 };
 
 export default app;
